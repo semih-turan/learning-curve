fccb5223dc4010f5194b69313bf4913d1308fc36
Semih Turan <semituran@gmail.com>
2023-11-09 15:17:21 +0300

    add dynamic values

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,11 +1,19 @@
+const reactDescriptions = ['Fundamental','Cruical','Core'];
+
+function genRandomInt(maxNumber){
+  return Math.floor(Math.random() * (maxNumber + 1));
+}
+
 // 1st Custom component
 function Header() {
+  const description = reactDescriptions[genRandomInt(3)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
