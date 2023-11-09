35d9932464810f9946203d1999e7b2c448c9dea2
Semih Turan <semituran@gmail.com>
2023-11-09 15:21:53 +0300

    add html attibutes dynamically and loading image file

--- React/01-starting-project/src/App.jsx
+++ React/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental','Cruical','Core'];
 
 function genRandomInt(maxNumber){
@@ -10,7 +12,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
