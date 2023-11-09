9b502cacb9d8abf0c2fe7c3fadd88785deca15e9
Semih Turan <semituran@gmail.com>
2023-11-09 18:45:26 +0300

    add event listener

--- React/01-starting-project/src/components/TabButton/TabButton.jsx
+++ React/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,7 +1,22 @@
 import './TabButton.css';
 
+/* Alternative 1 
 export default function TabButton(props) {
     return (
         <li><button>{props.children}</button></li>
+    );
+}
+*/
+
+/* Alternative 2 */
+export default function TabButton({children}) {
+    function handleClick() {
+        console.log('Clicked');
+    };
+
+    return (
+        <li>
+            <button onClick={handleClick} >{children}</button>
+        </li>
     );
 }
