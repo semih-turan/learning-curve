dfe9e8b3e6282dc245820f7aa87cac494a2c0835
Semih Turan <semituran@gmail.com>
2024-01-06 19:27:28 +0300

    add header components

---
+++ React/03-investment-calculator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+
+export default function Header(){
+    return(
+        <header id="header">
+            <img src={logo} alt="investment calculator logo image" />    
+            <h1>React Investment Calculator</h1>
+        </header>
+    )
+}
