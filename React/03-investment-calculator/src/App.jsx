20f1c76daa0f938f173c9addd899948480a01ca1
Semih Turan <semituran@gmail.com>
2024-01-06 19:38:22 +0300

    add userinput component

--- React/03-investment-calculator/src/App.jsx
+++ React/03-investment-calculator/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
+      <UserInput />
     </>
   )
 }
