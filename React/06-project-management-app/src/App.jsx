9b907bd0b84d6ad2080f01625a27c4b277137010
semih turan <semituran@gmail.com>
2024-01-14 23:45:20 +0300

    managing state to switch btw components

--- React/06-project-management-app/src/App.jsx
+++ React/06-project-management-app/src/App.jsx
@@ -2,12 +2,36 @@ import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
+import {useState} from 'react';
+
 function App() {
-  return (
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectSidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
