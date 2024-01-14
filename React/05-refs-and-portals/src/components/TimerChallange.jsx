841f3c8607c05f3cdff4a35509a557b65c5a6a09
semih turan <semituran@gmail.com>
2024-01-14 18:09:21 +0300

    Enhancing the Demo App "Result Modal" and closing the Modal via the ESC(escape) key

--- React/05-refs-and-portals/src/components/TimerChallange.jsx
+++ React/05-refs-and-portals/src/components/TimerChallange.jsx
@@ -6,20 +6,22 @@ export default function TimerChallange({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timeRemaining, setTimeRemaining] = useState(targetTime*1000);
-  
-  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
-  
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-  if(timeRemaining <= 0) {
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime *1000);
     dialog.current.open();
   }
- 
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
   function handleStart() {
     timer.current = setInterval(() => {
-      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
     }, 10);
   }
 
@@ -30,7 +32,12 @@ export default function TimerChallange({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
