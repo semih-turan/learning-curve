862dcaf464406a1ad971f5d7e3f565c242fd47aa
semih turan <semituran@gmail.com>
2024-01-14 22:40:07 +0300

    add Newproject component and reusable Input component

--- React/06-project-management-app/src/App.jsx
+++ React/06-project-management-app/src/App.jsx
@@ -1,9 +1,11 @@
-import ProjectSidebar from "./components/ProjectSidebar";
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8 ">
+    <main className="h-screen my-8 flex gap-8 ">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
