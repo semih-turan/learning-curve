cd9ade5a7e7f834e90e92ea006f89b6b0908e8fe
semih turan <semituran@gmail.com>
2024-01-18 23:36:29 +0300

    fix CartContext import

--- React/07-shopping-app/src/App.jsx
+++ React/07-shopping-app/src/App.jsx
@@ -4,8 +4,7 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import {CartContext} from "./store/CartContext.jsx";
-
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
